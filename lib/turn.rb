0cb21f8fdafcd2abf727de09a82a9655298d15e3
Ahmed Abououf <aabuauf@yahoo.com>
2019-07-12 18:30:15 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,7 +6,22 @@ def input_to_index(input)
 input = gets.strip
 index = input.to_i
 end 
+def position_taken?(board, index)
+  # basic solution:
 
+  # taken = nil
+  # if (board[index] ==  " " || board[index] == "" || board[index] == nil)
+  #   taken = false
+  # else
+  #   taken = true
+  # end
+  # taken
+
+  # advanced solution w/ ternary operator
+  (board[index] ==  " " || board[index] == "" || board[index] == nil) ?
+    false : true
+
+end
 def valid_move?(board,index)
   if (board[index] == "X" || board[index]) == "O" && index.between?(0,8)
     true
